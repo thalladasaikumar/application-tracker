680bc1194b0da731467b8e18ef7bdd9595fe08ad
abhinay <abhinay7777@gmail.com>
2021-01-11 21:27:58 -0800

    Revert "UI for form has been finished"
    This reverts commit 15301232ba32737212720bb6e314243a2a1d95ce.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
 import './App.css';
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import {Header}  from "./Components/Header";
 
 function App() {
   return (
-    <div>
-      <Header/>
-        <Form>
-          <Form.Group controlId="formBasicDate">
-          <Form.Label>Date</Form.Label>
-          <Form.Control type="text" placeholder="MM/DD/YYYY" />          
-          </Form.Group>
-
-          <Form.Group controlId="formBasicCompany">
-          <Form.Label>Company</Form.Label>
-          <Form.Control type="text" placeholder="Name of the company" />
-          </Form.Group>
-          
-          <Form.Group controlId="formBasicPosition">
-          <Form.Label>Position</Form.Label>
-          <Form.Control type="text" placeholder="Position applied for" />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicJobDescription">
-          <Form.Label>Job desciption</Form.Label>
-          <Form.Control as="textarea" placeholder="Responsibilites and skills highlighted in job description" />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicJobLink">
-          <Form.Label>Posting link</Form.Label>
-          <Form.Control type="text" placeholder="web link of job posting" />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicAppliedThrough">
-          <Form.Label>Company</Form.Label>
-          <Form.Control type="text" placeholder="Applied through recruiter if any" />
-          </Form.Group>
-          
-          <Button variant="primary" type="submit">
-          Submit
-          </Button>
-        </Form>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to Application Tracker!!!!!!
+        </p>
+      </header>
     </div>
   );
 }
